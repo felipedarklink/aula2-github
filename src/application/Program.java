8f5bcf68bb799828c373494bce9c29112f2a6907
Felipe Silva <felipeosilva@hotmail.com>
2019-09-10 12:56:51 -0300

    ENUM

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,24 @@
 package application;
 
+import java.util.Date;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("TESTE");
-		System.out.println("TESTE2");
-		System.out.println("TESTE3");
+		
+		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+		
+		System.out.println(order);
 
+		OrderStatus os1 = OrderStatus.DELIVERED;
+		
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+		
+		System.out.println(os1);
+		System.out.println(os2);
 	}
 
 }
